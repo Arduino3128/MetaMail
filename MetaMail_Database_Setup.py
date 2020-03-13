5f1a758f1c5040d30b614adbd89f22c99956ece6
Kanad Nemade <kanadnemade88@gmail.com>
2020-03-13 13:00:14 +0530

    Update MetaMail_Database_Setup.py

--- MetaMail_Database_Setup.py
+++ MetaMail_Database_Setup.py
@@ -1,43 +1,45 @@
 import time
 import subprocess
+import platform
+osident=platform.system()
 try:
    import mysql.connector
 except:
    print("MySQL Connector not found! Starting 'mysql-connector-python' Module installing Procedure!")
-   piploc=input("Enter the directory path of Python: ")
-   subprocess.call("cd %s/Scripts && pip install mysql-connector-python"%piploc, shell=True)
-   subprocess.call("python MetaMail_Database_Setup.py", shell=True)
+   if osident=="Windows":
+       piploc=input("Enter the directory path of Python: ")
+       subprocess.call("cd %s/Scripts && pip install mysql-connector-python "%piploc, shell=True)
+       subprocess.call("MetaMail_Database_setup.py", shell=True)
+   elif osident=="Linux" or osident=="Darwin":
+       subprocess.call("pip install mysql-connector-python", shell=True)
+       subprocess.call("python3 MetaMail_Database_setup.py", shell=True)
+   else:
+       print("Unknown OS, Try Installing 'mysql-connector-python' manually!")
    exit()
    time.sleep(5)
 print(" ")
 print("Welcome to MetaMail Database Setup!")
 print("")
 time.sleep(2)
-select=input("Enter 1 for LAN(localhost) or 2 for WAN(Internet) or Press Just 'Enter' to Customize Connection ")
-if select=="1":
-    userinput=input("Enter Username(Contact Admin For Username) ")
-    passwdinput=input("Enter Password(Contact Admin For Password) ")
-    portinput=input("Enter Port No. of the Server(Contact Admin For Port No.) ")
-    hostinput="localhost"
-elif select=="2":
-    userinput=input("Enter Username(Contact Admin For Username) ")
-    passwdinput=input("Enter Password(Contact Admin For Password) ")
-    portinput=input("Enter Port No. of the Server(Contact Admin For Port No.) ")
-    hostinput="o.tcp.ngrok.io"
-
-else:
-    userinput=input("Enter Username(Contact Admin For Username) ")
-    passwdinput=input("Enter Password(Contact Admin For Password) ")
-    portinput=input("Enter Port No. of the Server(Contact Admin For Port No.) ")
-    hostinput=input("Enter host IP ")
-    
-dbc = mysql.connector.connect(
-  host=hostinput,
-  user=userinput,
-  passwd=passwdinput,
-  port=portinput
-  auth_plugin='caching_sha2_password'
-  )
+userinput=input("Enter Username(Contact Admin For Username) ")
+passwdinput=input("Enter Password(Contact Admin For Password) ")
+portinput=input("Enter Port No. of the Server(Contact Admin For Port No.) ")
+hostinput=input("Enter host IP ")
+   
+def connerr():
+    logo()
+    print("Connection Error! Maybe the Server is down! Try Again Later!")
+    time.sleep(4)
+    exit()
+try:
+    dbc = mysql.connector.connect(
+    host=hostinput,
+    user=userinput,
+    passwd=passwdinput,
+    port=portinput,
+    auth_plugin='caching_sha2_password')
+except:
+    connerr()
 c = dbc.cursor()
 print("Please Wait Setup is Running.....")
 c.execute("CREATE DATABASE User;")
